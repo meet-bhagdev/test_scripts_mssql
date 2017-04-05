1a730f8ed558cdd2d04a3808f79a21d5dd307bb5
Meet Bhagdev <mebha@microsoft.com>
2017-04-05 16:24:55 -0700

    Create start10back.sql

---
+++ start10back.sql
@@ -0,0 +1,14 @@
+USE [msdb]
+GO
+
+
+EXEC dbo.sp_start_job N'BackupDB1'
+EXEC dbo.sp_start_job N'BackupDB2'
+EXEC dbo.sp_start_job N'BackupDB3'
+EXEC dbo.sp_start_job N'BackupDB4'
+EXEC dbo.sp_start_job N'BackupDB5'
+EXEC dbo.sp_start_job N'BackupDB6'
+EXEC dbo.sp_start_job N'BackupDB7'
+EXEC dbo.sp_start_job N'BackupDB8'
+EXEC dbo.sp_start_job N'BackupDB9'
+EXEC dbo.sp_start_job N'BackupDB10'
